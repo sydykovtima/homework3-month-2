c424e5249de05f8bd5fbdc5768e0bd2cb8f8e7f9
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2022-11-26 13:47:02 +0600

    Update patient.swift

--- homework 3 month 2/patient.swift
+++ homework 3 month 2/patient.swift
@@ -21,18 +21,3 @@ class Patient {
         self.happybirthday = happybirthday
     }
 }
-//class Patient {
-//    var name:String?
-//    var surename:String?
-//    var happybirthday:Double?
-//    init() {
-//        self.name = nil
-//        self.surename = nil
-//        self.happybirthday = nil
-//    }
-//    init(name:String,surename:String,happybirthday:Double) {
-//        self.name = name
-//        self.surename = surename
-//        self.happybirthday = happybirthday
-//    }
-//}
